462892104b60f9e0b3795adc28a5bb2c0777e971
Quentin <zinatiix@gmail.com>
2023-11-14 20:54:36 +0100

    ça marche inchallah 2

--- lottery/views/EventsPage.tsx
+++ lottery/views/EventsPage.tsx
@@ -6,156 +6,96 @@ import { theme } from "../theme.config.json";
 import { JsonRpcProvider, Contract } from "ethers";
 
 function EventsPage() {
+  const contractAddress = "0x05f37fd91676a73c6d02f7E6d13286EEE9f51b80";
+  const contractABI = [
+    { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+    {
+      inputs: [],
+      name: "enter",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getOwner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getPlayers",
+      outputs: [
+        { internalType: "address payable[]", name: "", type: "address[]" },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getPotBalance",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getRandomNumber",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "lottery", type: "uint256" }],
+      name: "getWinnerByLottery",
+      outputs: [{ internalType: "address payable", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      name: "lotteryHistory",
+      outputs: [{ internalType: "address payable", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "lotteryId",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address payable", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "pickWinner",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      name: "players",
+      outputs: [{ internalType: "address payable", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
 
-  // const contractAddress = "0xEeB3D9c75eF53b92a5BaCd8BeeFd8d7f371442eA";
-  // const contractABI = [
-  //   {
-  //     inputs: [],
-  //     stateMutability: "nonpayable",
-  //     type: "constructor",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "enter",
-  //     outputs: [],
-  //     stateMutability: "payable",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "getPlayers",
-  //     outputs: [
-  //       {
-  //         internalType: "address payable[]",
-  //         name: "",
-  //         type: "address[]",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "getPotBalance",
-  //     outputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "getRandomNumber",
-  //     outputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "lottery",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     name: "getWinnerByLottery",
-  //     outputs: [
-  //       {
-  //         internalType: "address payable",
-  //         name: "",
-  //         type: "address",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     name: "lotteryHistory",
-  //     outputs: [
-  //       {
-  //         internalType: "address payable",
-  //         name: "",
-  //         type: "address",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "lotteryId",
-  //     outputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "owner",
-  //     outputs: [
-  //       {
-  //         internalType: "address payable",
-  //         name: "",
-  //         type: "address",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [],
-  //     name: "pickWinner",
-  //     outputs: [],
-  //     stateMutability: "payable",
-  //     type: "function",
-  //   },
-  //   {
-  //     inputs: [
-  //       {
-  //         internalType: "uint256",
-  //         name: "",
-  //         type: "uint256",
-  //       },
-  //     ],
-  //     name: "players",
-  //     outputs: [
-  //       {
-  //         internalType: "address payable",
-  //         name: "",
-  //         type: "address",
-  //       },
-  //     ],
-  //     stateMutability: "view",
-  //     type: "function",
-  //   },
-  // ];
+  const [lotteryContract, setlotteryContract] = useState<any>(null);
 
-  // const [lotteryContract, setlotteryContract] = useState<any>(null);
+  const provider = new JsonRpcProvider("https://rpc.sepolia.org");
+  const contract = new Contract(contractAddress, contractABI, provider);
 
-  // const provider = new JsonRpcProvider("https://rpc.sepolia.org");
-  // const contract = new Contract(contractAddress, contractABI, provider);
+  const data = contract.getOwner().then((data) => console.log(data));
 
   // console.log(contract);
   return (
